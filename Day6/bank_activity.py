e93cc1f5d83500da1816d0df23ea5d0e467926d1
Navya-SG <Navya.Gokila@Coforge.com>
2025-07-18 10:14:27 +0530

    message

--- Day6/bank_activity.py
+++ Day6/bank_activity.py
@@ -5,7 +5,10 @@ print("Navya Sathishkumar",*acc_info[1:])
 
 acc_info=("Navya","1XXXX","AX123",2500.50,2000,100)
 print(*acc_info)
-print(*acc_info[:3],2500.50-100,2000,100-100)
+print(*acc_info[1:4],acc_info[4]+100,acc_info[5])
 
-acc_info=("Navya","1XXXX","AX123",2500.50,2000,100)
-print(*acc_info[:5],0)
+acc_info=("Navya","1XXXX","AX123",2500.50,2000,200)
+debit = int(input("Enter amount to debit:"))
+print(*acc_info)
+print(*acc_info[:3],acc_info[3]-debit,acc_info[4],acc_info[5]-debit)
+
