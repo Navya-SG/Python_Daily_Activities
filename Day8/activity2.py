fa03d72725d348ac392fbf49e77c4e854ba43399
Navya-SG <Navya.Gokila@Coforge.com>
2025-07-21 16:34:46 +0530

    commit

--- Day8/activity2.py
+++ Day8/activity2.py
@@ -4,3 +4,6 @@ print(sentence[1:-1])
 sentence="Python make you think differently".split()
 print(sentence[1],sentence[2],sentence[3])
 
+sentence="Python make you think differently"
+first, *middle, last = sentence.split()
+print(middle)
--- try.py
+++
@@ -1,7 +0,0 @@
-# d={1:'a', 2: 'b'}
-# print(d.keys)
-
-fruit_prices={"elppa":100,
-              "ananab":60,
-              "egnaro":80}
-print(fruit_prices[input("Enter fruit name:").lower()[::-1]])
