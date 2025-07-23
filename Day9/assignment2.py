1aca446c991ddcd5ae9a45e1772fd46e76b1691a
Navya-SG <Navya.Gokila@Coforge.com>
2025-07-23 18:17:44 +0530

    commit day10

--- Day9/assignment2.py
+++ Day9/assignment2.py
@@ -1,9 +1,8 @@
-# data=[('A',1,2),('B',3),('C',4,5,6),('D'),('E',7,8)]
-# extract=data[0][0],data[1][0],data[2][0],data[3][0],data[4][0]
-# a=sorted((len(data[0]),len(data[1]),len(data[2]),len(data[3]),len(data[4])))
-# print(sorted((len(data[0]) and data[0][]0,len(data[1]),len(data[2]),len(data[3]),len(data[4]))))
-
-# #['D', 'B', 'A', 'E', 'C']
-# data=[('A',1,2),('B',3),('C',4,5,6),('D'),('E',7,8)]
-
-# print(res)
+data =[('A', 1, 2),
+       ('B', 3),
+       ('C', 4, 5, 6),
+       ('D',),
+       ('E', 7, 8)]
+sorted_data = sorted(data, key=len)
+result = list(list(zip(*sorted_data))[0])
+print(result)
--- Day9/navya_reverse_lookup.py
+++ Day9/navya_reverse_lookup.py
@@ -3,3 +3,11 @@ scores={"anita":92,
         "kiran":76,
         "zoya":88}
 print(dict(zip(scores.values(),scores.keys())).get(int(input("enter score:"))))
+
+#final
+scores={"Anita":92,
+        "Ravi":85,
+        "Kiran":76,
+        "Zoya":88
+}
+print(dict(zip(scores.values(),scores.keys())).get(int(input("Enter score:")),"Not found"))
