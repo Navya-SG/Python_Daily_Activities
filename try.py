860163a03ef2d59cd301171272b9344d8be4d37a
Navya-SG <Navya.Gokila@Coforge.com>
2025-07-23 10:24:21 +0530

    Update try.py

--- try.py
+++ try.py
@@ -1,8 +1,24 @@
-# fruit_prices = {"elppa": 100,
-#                 "ananab": 60, 
-#                 "egnaro": 80}  
-                
-# fruit_name = "".join(sorted(input("Enter fruit name: "))) 
-# sorted_keys = "".join(map(sorted, fruit_prices.keys()))
-# (fruit_name in sorted_keys and print(fruit_prices.get(fruit_name,"None")))
+#sol4
+fruit_prices = {"elppa": 100,
+                "ananab": 60, 
+                "egnaro": 80}  
 
+sorted_fruits = {"apple":fruit_prices.get("elppa"),
+                 "banana":fruit_prices.get("ananab"),
+                 "orange":fruit_prices.get("egnaro")}
+
+fruit_name = input("Enter fruit name: ") 
+print(sorted_fruits.get(fruit_name,"None"))
+
+#sol5
+fruit_prices = {"elppa": 100, 
+                "ananab": 80, 
+                "egnaro": 60}
+
+fruit_name = ''.join(sorted(input("Enter fruit name:")))
+
+updated_fruit_prices = {''.join(sorted("elppa")): fruit_prices["elppa"],
+                        ''.join(sorted("ananab")): fruit_prices["ananab"],
+                        ''.join(sorted("egnaro")): fruit_prices["egnaro"]}
+
+print(updated_fruit_prices.get(fruit_name, "not found"))
