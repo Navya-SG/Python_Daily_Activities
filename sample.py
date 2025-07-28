8e2f78b8a71eee91340d75493f1d961dbbeee65c
Navya-SG <Navya.Gokila@Coforge.com>
2025-07-28 10:35:37 +0530

    commit

--- sample.py
+++ sample.py
@@ -1,34 +1,3 @@
 
-# '''
-# reversed dict (keys,values)?
-# how to keep/store ordered value in set
-# how to store duplicates in set
-# scrambled - membership
-# walrus - dict
-# '''
-
-# original_dict = {'a': 1, 'b': 2, 'c': 3}
-# reversed_dict = dict(zip(original_dict.values(), original_dict.keys()))
-# print(reversed_dict)
-
-
-# # values = ['banana', 'apple', 'banana', 'cherry']
-# # ordered_set = set(dict.fromkeys(values))
-# # print(ordered_set)
-
-
-# # from collections import Counter
-# # values = ['apple', 'banana', 'apple', 'cherry']
-# # counter = Counter(values)
-# # print(counter)  # Output: Counter({'apple': 2, 'banana': 1, 'cherry': 1})
-
-# # duplicate_set = {'apple'*2, 'banana'*1, 'cherry,'*3}
-# # print(duplicate_set)
-
-
-a={'a':[1],
-   'b':{2},
-   'a':{c}}
-print(a)
 
 
