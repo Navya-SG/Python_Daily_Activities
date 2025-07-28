f77b5edf1d9d7803f9d68a6f025cfc1e15428ba1
Navya-SG <Navya.Gokila@Coforge.com>
2025-07-28 17:44:58 +0530

    commit

--- Day13/activity1.py
+++ Day13/activity1.py
@@ -3,7 +3,8 @@
 # if (marks - cutoff) :
 # 	print("pass")
 
+#final sol
 marks = int(input("enter marks out of 100:"))
 cutoff = int(input("enter cutoff mark:"))
-if ((marks - cutoff) // (abs(marks - cutoff) + 1)):
+if (marks-cutoff + abs(marks-cutoff)) // 2 is (marks-cutoff):
     print("pass")
