0fee72a2d2a08716be09870c1372e023f1189e98
Navya-SG <Navya.Gokila@Coforge.com>
2025-08-11 16:40:11 +0530

    commit

--- Practice/PANDAS/work.py
+++ Practice/PANDAS/work.py
@@ -1,4 +1,11 @@
+#join DataFrames (DF1.join(DF2))
 import pandas as pd
-data = {'a':100,'b':200,'c':300}
-s = pd.Series(data)
-print(s)
+df=pd.read_json("data.json")
+df1 = df.set_index('name')
+df2 = pd.DataFrame({
+    'grade': ['A', 'B'],
+}, index=['Navya', 'Gokila'])
+joined = df1.join(df2)
+print(joined) #set col as index and select row to that col as index to join values
+
+#FA2025_NAVYA
